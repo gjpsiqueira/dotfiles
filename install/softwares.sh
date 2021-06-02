1f7e8b4910dbaca41d487b7c002322d39c2adb93
Gustavo Siqueira <gjpsiqueira@gmail.com>
2021-06-02 17:20:32 -0300

    add software list scripts

---
+++ install/softwares.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+source colors.sh
+
+#  Brew Cask ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+msg_install "Installing apps with brew cask"
+brew cask
+
+cask=(
+  "iterm2"
+  "visual-studio-code"
+  "spotify"
+  "coconutbattery"
+  "zoom"
+)
+
+for app in "${cask[@]}"; do
+  msg_install "Installing $app"
+  brew install --cask $app
+  msg_ok "$app"
+done
